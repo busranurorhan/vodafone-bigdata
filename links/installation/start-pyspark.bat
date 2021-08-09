906a2dc81ad028d28fae74197bb25ce7d7e9afba
Ahmet Demirelli <ahmetdemirelli@gmail.com>
2021-08-09 09:20:10 +0300

    Insstallation

---
+++ links/installation/start-pyspark.bat
@@ -0,0 +1,8 @@
+set PATH=%PATH%;C:\spark\spark-3.1.2-bin-hadoop3.2\bin
+set SPARK_HOME=C:\spark\spark-3.1.2-bin-hadoop3.2
+set HADOOP_HOME=C:\spark\spark-3.1.2-bin-hadoop3.2
+set JAVA_HOME="C:\Program Files\Java\jre1.8.0_241"
+set PYSPARK_DRIVER_PYTHON=jupyter
+set PYSPARK_DRIVER_PYTHON_OPTS=notebook
+set SPARK_LOCAL_HOSTNAME=localhost
+pyspark
